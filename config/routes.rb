93850c6e339f5d36c595d3e79cebb3868c3b8b79
Bergson Lima <bergsonsud@gmail.com>
2024-07-05 16:28:44 -0300

    feat: change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :recruiters, defaults: { format: :json } do
-    mount_devise_token_auth_for "Recruiter", at: "/"
+  namespace :v1 do
+    namespace :recruiters, defaults: { format: :json } do
+      mount_devise_token_auth_for "Recruiter", at: "/"
+      resources :jobs
+      resources :submissions, expect: %i[create]
+    end
+    resources :submissions, only: %i[create]
+    resources :jobs, only: %i[index show]
   end
 end
