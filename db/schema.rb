22cae6aab5705f71eaef5c8c9919b092fbe9e6e7
Bergson Lima <bergsonsud@gmail.com>
2024-07-05 16:21:52 -0300

    feat: update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_02_201641) do
+ActiveRecord::Schema.define(version: 2024_07_05_153444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "job_skills", force: :cascade do |t|
+    t.string "skill"
+    t.bigint "job_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["job_id"], name: "index_job_skills_on_job_id"
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "status", default: 0
+    t.bigint "recruiter_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["description"], name: "index_jobs_on_description"
+    t.index ["end_date"], name: "index_jobs_on_end_date"
+    t.index ["recruiter_id"], name: "index_jobs_on_recruiter_id"
+    t.index ["start_date"], name: "index_jobs_on_start_date"
+    t.index ["status"], name: "index_jobs_on_status"
+    t.index ["title"], name: "index_jobs_on_title"
+  end
 
   create_table "recruiters", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -38,4 +63,19 @@ ActiveRecord::Schema.define(version: 2024_07_02_201641) do
     t.index ["uid", "provider"], name: "index_recruiters_on_uid_and_provider", unique: true
   end
 
+  create_table "submissions", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "mobile_phone"
+    t.string "resume"
+    t.bigint "job_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email", "job_id"], name: "index_submissions_on_email_and_job_id", unique: true
+    t.index ["job_id"], name: "index_submissions_on_job_id"
+  end
+
+  add_foreign_key "job_skills", "jobs"
+  add_foreign_key "jobs", "recruiters"
+  add_foreign_key "submissions", "jobs"
 end
