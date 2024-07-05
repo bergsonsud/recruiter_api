fe7d0b3207e5f57b196d64d98091472786bccc36
Bergson Lima <bergsonsud@gmail.com>
2024-07-05 16:29:01 -0300

    feat: add method auth_headers

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -7,6 +7,13 @@ module RequestHelpers
       "HTTP_ACCEPT" => "application/json",
     }
   end
+
+  def auth_headers(recruiter)
+    headers = json_headers
+    post "/v1//recruiters/sign_in", params: { email: recruiter.email, password: recruiter.password }, headers: headers
+    headers["Authorization"] = response.headers["Authorization"]
+    headers
+  end
 end
 
 RSpec.configure do |config|
